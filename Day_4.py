16a6a535809268bb2139b5c130ba858f0d69ba74
Sung Kim <stwkim103@gmail.com>
2025-01-31 16:53:17 -0500

    Add files via upload

--- Day_4.py
+++ Day_4.py
@@ -1,11 +1,11 @@
 import random
 
-weapon_choice = ''
+weapon_choice = 0
 computer_choice = random.randint(0, 2)
 player_choice = int(input("What do you choose?\nType 0 for Rock, 1 for Paper, or 2 for Scissors: "))
 
 if player_choice == 0 and computer_choice == 1:
-    print("Player loses!")
+    print(f"Player loses! Player chose {player_choice} and Computer chose {computer_choice}")
     if player_choice == 0 and computer_choice == 2:
         print("Player wins!")
     if player_choice == 0 and computer_choice == 0:
@@ -24,3 +24,5 @@ if player_choice == 0 and computer_choice == 1:
         print("It's a draw!")
     else:
         print("Please choose a valid option.")
+
+print(f"Player loses! Player chose {player_choice} and Computer chose {computer_choice}")
