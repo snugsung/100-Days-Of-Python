33effb0f53e4dd3cf8288772ea66e17638f9c4ec
Sung Kim <stwkim103@gmail.com>
2025-02-03 13:39:26 -0500

    Update Day_4.py

--- Day_4.py
+++ Day_4.py
@@ -1,6 +1,8 @@
 import random
 
-weapon_choice = 0
+computer_score = 0
+player_score = 0
+
 computer_choice = random.randint(0, 2)
 player_choice = int(input("What do you choose?\nType 0 for Rock, 1 for Paper, or 2 for Scissors: "))
 
